2eb6ecb9936f2985095e59c9d4167e394b70ddaa
huahchen <hua.helen.chen@gmail.com>
2015-02-23 17:16:55 -0500

    Randomized color of each character in title

--- Colorize/ViewController.swift
+++ Colorize/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorize: UITextField!
+    var mutateColor = NSMutableAttributedString(string: "Colorize")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for (var idx = 0; idx < mutateColor.length; idx++) {
+            var red = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+            var blue = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+            var green = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+            mutateColor.addAttribute(NSForegroundColorAttributeName, value: UIColor(red: red, green: green, blue: blue, alpha: 1.0), range: NSRange(location: idx, length: 1))
+        }
+        colorize.attributedText = mutateColor
     }
 
     override func didReceiveMemoryWarning() {
